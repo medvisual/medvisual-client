b2820f3ce2767a1176086ce005b761bbe6f732f2
palp1ix <arthach.2008@gmail.com>
2024-11-03 15:10:27 +0300

    User roles added

--- lib/src/repository/models/user/user.dart
+++ lib/src/repository/models/user/user.dart
@@ -1,13 +1,21 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:medvisual/src/repository/enums/user_roles_enum.dart';
 
 part 'user.g.dart';
 
 @JsonSerializable()
 class User {
-  User({required this.email, required this.password});
+  User(
+      {required this.email, required this.password, this.role = UserRole.user});
 
   final String email;
   final String password;
+  @JsonKey(includeToJson: false, includeFromJson: false)
+  final UserRole role;
+
+  bool get isAdmin => role == UserRole.admin ? true : false;
+  bool get isDoctor => role == UserRole.doctor ? true : false;
+  bool get isUser => role == UserRole.user ? true : false;
 
   /// Connect the generated [_$UserFromJson] function to the `fromJson`
   /// factory.
